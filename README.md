98ba7a197f35c078a5d14077ae126b242d2992d9
karol <karol.bednarz@streamuk.com>
2016-08-01 09:55:30 +0200

    Initial commit with the script
    Script added to the repository
    Updated README.md file with the instructions

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bitbucket-deployer
 Bash script for deploying directly from bitbucket to your server
+
+## USAGE
+
+1. Copy DeployBranch.sh to the destination you want to deploy 
+2. run script like that 
+
+```js
+sh DeployBranch.sh master MyDeploy  https://bitbucket.org/path/toProject/get/ user.name@mail.com password
+```
+
+where:
+@param1 branch of the player on bitbucket 
+@param2 folder to extract the player 
+@param3 bitbucket project URL
+@param4 userID on bitbucket
+@param5 user Password on bitbucket
---
+++ bitbacket-deployer.sh
@@ -0,0 +1,61 @@
+# Script download player from bitbucket repositor
+
+# @param1 branch of the player on bitbucket 
+# @param2 folder to extract the player 
+# @param3 bitbucket project URL
+# @param4 userID on bitbucket
+# @param5 user Password on bitbucket
+
+defaultBranch="master"
+
+
+if [ -z "$2" ]
+  then
+    destination="$defaultBranch"
+  else
+    destination=$2
+fi
+
+
+if [ -z "$1" ]
+  then        
+    branch=$defaultBranch    
+  else    
+    branch=$1        
+fi
+
+echo [info] downloading branch $branch
+
+file=$branch'.zip'
+
+if [ -f $file ] ; then
+   rm $file
+fi
+
+dpa=$3$file
+echo $dpa
+
+sudo wget --user=$4 --password=$5 $dpa
+
+unzip -o $branch'.zip' -d $destination
+
+shopt -s dotglob # To include hidden files in the move command
+
+
+function moveSub {
+  local dest=$1  
+  if [ `ls $dest | wc -l` == 1 ]; then    
+    local subdir=`ls $1`    
+    moveSub "$dest/$subdir"    
+    mv $dest/$subdir/* $dest/    
+    rmdir $dest/$subdir    
+  fi
+}
+
+moveSub "$destination"
+
+if [ -f $file ] ; then
+  rm $file
+fi
+
+echo [info] Player downloaded to $destination
