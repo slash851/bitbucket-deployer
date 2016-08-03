bf37a48693b23d8c846eb911468f2a157abd1274
karol <karol.bednarz@streamuk.com>
2016-08-03 12:01:50 +0200

    Creating backup of previous version

--- bitbacket-deployer.sh
+++ bitbacket-deployer.sh
@@ -1,7 +1,7 @@
 # Script download player from bitbucket repositor
 
-# @param1 branch of the player on bitbucket 
-# @param2 folder to extract the player 
+# @param1 branch of the player on bitbucket
+# @param2 folder to extract the player
 # @param3 bitbucket project URL
 # @param4 userID on bitbucket
 # @param5 user Password on bitbucket
@@ -18,10 +18,25 @@ fi
 
 
 if [ -z "$1" ]
-  then        
-    branch=$defaultBranch    
-  else    
-    branch=$1        
+  then
+    branch=$defaultBranch
+  else
+    branch=$1
+fi
+
+#create backup of previous version
+if [ -d "$destination" ]; then
+  name=$destination-$(date '+%d-%b-%Y')
+  echo $name
+  if [[ -e $name ]] ; then
+      i=0
+      while [[ -e $name-$i ]] ; do
+          let i++
+      done
+      name=$name-$i
+  fi
+  touch $name.ext
+  mv $destination $name
 fi
 
 echo [info] downloading branch $branch
@@ -43,12 +58,12 @@ shopt -s dotglob # To include hidden files in the move command
 
 
 function moveSub {
-  local dest=$1  
-  if [ `ls $dest | wc -l` == 1 ]; then    
-    local subdir=`ls $1`    
-    moveSub "$dest/$subdir"    
-    mv $dest/$subdir/* $dest/    
-    rmdir $dest/$subdir    
+  local dest=$1
+  if [ `ls $dest | wc -l` == 1 ]; then
+    local subdir=`ls $1`
+    moveSub "$dest/$subdir"
+    mv $dest/$subdir/* $dest/
+    rmdir $dest/$subdir
   fi
 }
 
